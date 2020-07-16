552fb7965d3e8197d128dae6a9bd6913462f1b41
Daniel Ramirez <dxas90@gmail.com>
2020-07-16 21:52:29 +0300

    Added test

--- main.go
+++ main.go
@@ -2,12 +2,67 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+func withTracing(next http.HandlerFunc) http.HandlerFunc {
+	return func(response http.ResponseWriter, request *http.Request) {
+		defer log.Printf("[%s] %q", request.Method, request.URL.String())
+		// log.Printf("Tracing request for %s", request.RequestURI)
+		next.ServeHTTP(response, request)
+	}
+}
+
+func withLogging(next http.HandlerFunc) http.HandlerFunc {
+	return func(response http.ResponseWriter, request *http.Request) {
+		log.Printf("Logged connection from %s", request.RemoteAddr)
+		next.ServeHTTP(response, request)
+	}
+}
+
+func use(h http.HandlerFunc, middleware ...func(http.HandlerFunc) http.HandlerFunc) http.HandlerFunc {
+	for _, m := range middleware {
+		h = m(h)
+	}
+	return recoverHandler(h)
+}
+
+func recoverHandler(next http.HandlerFunc) http.HandlerFunc {
+	return func(response http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				http.Error(response, http.StatusText(500), 500)
+			}
+		}()
+
+		next.ServeHTTP(response, request)
+	}
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    // A very simple health check.
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+
+    // In the future we could report back on the status of our DB, or our cache
+    // (e.g. Redis) by performing a simple PING, and include them in the response.
+    fmt.Fprintf(w, `{"alive": true}`)
+}
+
+func pongHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "pong")
+}
+
+func helloHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	})
+
+	http.Handle("/", use(helloHandler, withLogging, withTracing))
+	http.Handle("/ping", use(pongHandler, withLogging, withTracing))
+	http.Handle("/healthz", use(healthCheckHandler, withLogging, withTracing))
 	http.ListenAndServe(":8080", nil)
 }
---
+++ main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloRoute(t *testing.T) {
+	handler := http.HandlerFunc(helloHandler)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Contains(t, w.Body.String(), "Hello")
+}
+
+func TestPingRoute(t *testing.T) {
+	handler := http.HandlerFunc(pongHandler)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestHealthRoute(t *testing.T) {
+	handler := http.HandlerFunc(healthCheckHandler)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthz", nil)
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Contains(t, w.Body.String(), "alive")
+}
