02a8dc4988cf4b9aec6bdf8c2434aa86f219bdd0
Daniel Ramirez <dxas90@gmail.com>
2020-07-17 17:55:42 +0300

    Added tracing

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module learn
 
 go 1.13
+
+require github.com/uber/jaeger-client-go v2.25.0+incompatible
+
+require (
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/stretchr/testify v1.6.1
+	github.com/uber/jaeger-lib v2.2.0+incompatible
+	go.mongodb.org/mongo-driver v1.3.5
+	go.uber.org/atomic v1.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-lib/metrics"
+
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
 )
+
+var tracer opentracing.Tracer
 
 func withTracing(next http.HandlerFunc) http.HandlerFunc {
 	return func(response http.ResponseWriter, request *http.Request) {
@@ -42,27 +51,60 @@ func recoverHandler(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    // A very simple health check.
-    w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "application/json")
+	span := tracer.StartSpan("healthchek")
+	// A very simple health check.
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 
-    // In the future we could report back on the status of our DB, or our cache
-    // (e.g. Redis) by performing a simple PING, and include them in the response.
-    fmt.Fprintf(w, `{"alive": true}`)
+	// In the future we could report back on the status of our DB, or our cache
+	// (e.g. Redis) by performing a simple PING, and include them in the response.
+	fmt.Fprintf(w, `{"alive": true}`)
+
+	span.Finish()
 }
 
 func pongHandler(response http.ResponseWriter, request *http.Request) {
+	span := tracer.StartSpan("pong")
 	fmt.Fprintf(response, "pong")
+	span.Finish()
 }
 
 func helloHandler(response http.ResponseWriter, request *http.Request) {
+	span := tracer.StartSpan("hello")
 	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
+	span.Finish()
 }
 
 func main() {
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg := jaegercfg.Configuration{
+		ServiceName: "learn",
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	tracer, closer, _ := cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	// Set the singleton opentracing.Tracer with the Jaeger tracer.
+	opentracing.SetGlobalTracer(tracer)
+	defer closer.Close()
 
 	http.Handle("/", use(helloHandler, withLogging, withTracing))
 	http.Handle("/ping", use(pongHandler, withLogging, withTracing))
-	http.Handle("/healthz", use(healthCheckHandler, withLogging, withTracing))
+	http.Handle("/healthz", use(healthCheckHandler))
 	http.ListenAndServe(":8080", nil)
 }
